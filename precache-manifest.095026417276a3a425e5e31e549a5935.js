b50e039ba0af682c0a44d015980109e13a58e065
nayan947 <mehtanayan730@gmail.com>
2024-10-10 14:30:16 +0530

    Updates

---
+++ precache-manifest.095026417276a3a425e5e31e549a5935.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccc22d97f1d3a66cde87453f7986fd1",
+    "url": "/devopsPortfolio/index.html"
+  },
+  {
+    "revision": "4e24e014e1284f22cf43",
+    "url": "/devopsPortfolio/static/css/main.7e355d10.chunk.css"
+  },
+  {
+    "revision": "debf18af16951f97f65c",
+    "url": "/devopsPortfolio/static/js/2.a621215e.chunk.js"
+  },
+  {
+    "revision": "4e24e014e1284f22cf43",
+    "url": "/devopsPortfolio/static/js/main.02886950.chunk.js"
+  },
+  {
+    "revision": "67a64b8db61119967179",
+    "url": "/devopsPortfolio/static/js/runtime-main.ddd9a710.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/devopsPortfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/devopsPortfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/devopsPortfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/devopsPortfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/devopsPortfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/devopsPortfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/devopsPortfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/devopsPortfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "16559172ca1d4d552390b48f19e102ba",
+    "url": "/devopsPortfolio/static/media/address_image.16559172.svg"
+  },
+  {
+    "revision": "85895fbfaaf02afe4759a9564a5f1cdf",
+    "url": "/devopsPortfolio/static/media/animated_logo.85895fbf.svg"
+  },
+  {
+    "revision": "908ba3891b64f1f211f5736dd2d112b8",
+    "url": "/devopsPortfolio/static/media/animated_nayan.908ba389.jpg"
+  },
+  {
+    "revision": "f6d309e3324f6965c01afa12d2d151ba",
+    "url": "/devopsPortfolio/static/media/aws_logo.f6d309e3.png"
+  },
+  {
+    "revision": "058eca4cbdc03a1dad9a6bb943ac7831",
+    "url": "/devopsPortfolio/static/media/blogs_image.058eca4c.svg"
+  },
+  {
+    "revision": "cacbc42b661f39344af81c631d8d36a9",
+    "url": "/devopsPortfolio/static/media/cloud_infrastructure.cacbc42b.svg"
+  },
+  {
+    "revision": "fee92c18b1cfbd1faa704c51047f113d",
+    "url": "/devopsPortfolio/static/media/codeInLogo.fee92c18.png"
+  },
+  {
+    "revision": "5f915405511f649dad433a6db9adbff4",
+    "url": "/devopsPortfolio/static/media/contactMail.5f915405.png"
+  },
+  {
+    "revision": "ebf35d7f33ebbd4a4c6cff7e66e21fdb",
+    "url": "/devopsPortfolio/static/media/data_science.ebf35d7f.svg"
+  },
+  {
+    "revision": "75a7107b7739a46950f66de29779eb52",
+    "url": "/devopsPortfolio/static/media/deeplearning_ai_logo.75a7107b.png"
+  },
+  {
+    "revision": "3e23ca14e7a7c8a7b63d255466749000",
+    "url": "/devopsPortfolio/static/media/developerActivity.3e23ca14.svg"
+  },
+  {
+    "revision": "63f707f224c8a842ac928c36e3be879a",
+    "url": "/devopsPortfolio/static/media/dsc_logo.63f707f2.png"
+  },
+  {
+    "revision": "3bab7a9dc4324f1b2364d25a47fcfcf0",
+    "url": "/devopsPortfolio/static/media/education.3bab7a9d.svg"
+  },
+  {
+    "revision": "a00ede3f70759480e81f14f203edaa51",
+    "url": "/devopsPortfolio/static/media/experience.a00ede3f.svg"
+  },
+  {
+    "revision": "2936ce95f550e3feef7230ea543a4b60",
+    "url": "/devopsPortfolio/static/media/feelingProud.2936ce95.svg"
+  },
+  {
+    "revision": "fce6080726685cabdb4f75a0d8018325",
+    "url": "/devopsPortfolio/static/media/fullstack.fce60807.svg"
+  },
+  {
+    "revision": "0ee7e8c9444cdfefc10f6d86e18b9b5b",
+    "url": "/devopsPortfolio/static/media/gcp_logo.0ee7e8c9.png"
+  },
+  {
+    "revision": "6eeb2e810d0fd9f3ca2dcd72de228e68",
+    "url": "/devopsPortfolio/static/media/github_logo.6eeb2e81.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/devopsPortfolio/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "a6addc3fb6605480614b992ede8cd434",
+    "url": "/devopsPortfolio/static/media/googleAssistant.a6addc3f.svg"
+  },
+  {
+    "revision": "d3fccbe1db76b96f4f320d9c0f7da30c",
+    "url": "/devopsPortfolio/static/media/ibm_logo.d3fccbe1.png"
+  },
+  {
+    "revision": "571a8584e69ecfbe326c8b8385fbbf54",
+    "url": "/devopsPortfolio/static/media/iiitk_logo.571a8584.png"
+  },
+  {
+    "revision": "29152cf756a1facead5393c32e373371",
+    "url": "/devopsPortfolio/static/media/intel_logo.29152cf7.jpg"
+  },
+  {
+    "revision": "60ed0bd3f8b99ac1d29e74575f804aaa",
+    "url": "/devopsPortfolio/static/media/iu_logo.60ed0bd3.png"
+  },
+  {
+    "revision": "5b4a6449747ec61a6d9aa874f2a9ceba",
+    "url": "/devopsPortfolio/static/media/jsFramework.5b4a6449.svg"
+  },
+  {
+    "revision": "3d79fb02c987aa29151e1cfefcbe6470",
+    "url": "/devopsPortfolio/static/media/kubernetes_logo.3d79fb02.png"
+  },
+  {
+    "revision": "c597fbded5de799dfb9abc3fc4f1c26f",
+    "url": "/devopsPortfolio/static/media/legato_logo.c597fbde.png"
+  },
+  {
+    "revision": "83ac24fb968a474c6a63f7a4e9a8f62c",
+    "url": "/devopsPortfolio/static/media/manOnTable.83ac24fb.svg"
+  },
+  {
+    "revision": "37c8cd6048a45ca247c1254425391c75",
+    "url": "/devopsPortfolio/static/media/medicaps_logo.37c8cd60.png"
+  },
+  {
+    "revision": "069938a84edd6ebf383c318e9feb5d4c",
+    "url": "/devopsPortfolio/static/media/muffito_logo.069938a8.png"
+  },
+  {
+    "revision": "95a12a2b8c3149cce2175679755c4b4d",
+    "url": "/devopsPortfolio/static/media/nptel_logo.95a12a2b.png"
+  },
+  {
+    "revision": "414dfbd98516b39255a3e22ed90b3f7d",
+    "url": "/devopsPortfolio/static/media/portfolio.414dfbd9.gif"
+  },
+  {
+    "revision": "5faf790badc18fa9030f94836ece1df1",
+    "url": "/devopsPortfolio/static/media/programmer.5faf790b.svg"
+  },
+  {
+    "revision": "b8ba948796d7ab532673c5ed2f315e74",
+    "url": "/devopsPortfolio/static/media/projects_image.b8ba9487.svg"
+  },
+  {
+    "revision": "71408db71465afd742233e4a96f06526",
+    "url": "/devopsPortfolio/static/media/pwa.71408db7.png"
+  },
+  {
+    "revision": "26d86d8b2c78cbd80979654d4e328ec1",
+    "url": "/devopsPortfolio/static/media/sica_logo.26d86d8b.png"
+  },
+  {
+    "revision": "6ac91b161c2c9934a44f72d75d18142d",
+    "url": "/devopsPortfolio/static/media/talksCardBack.6ac91b16.svg"
+  },
+  {
+    "revision": "3fa5424232370e3d049c9555d9c440a4",
+    "url": "/devopsPortfolio/static/media/talksCardBackAlt.3fa54242.svg"
+  },
+  {
+    "revision": "0993e3246bf5146154391a0afef79b03",
+    "url": "/devopsPortfolio/static/media/tiktok_logo.0993e324.png"
+  },
+  {
+    "revision": "d63630893eb8cb64af65f395d393c481",
+    "url": "/devopsPortfolio/static/media/ui_ux_design.d6363089.svg"
+  }
+]);
