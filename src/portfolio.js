30fa6f5c1903c44dff9f8b7b6b3c5278ed4483db
nayan947 <mehtanayan730@gmail.com>
2024-10-21 19:42:12 +0530

    Change contact description

--- src/portfolio.js
+++ src/portfolio.js
@@ -463,8 +463,8 @@ const contactPageData = {
     title: "Contact Me",
     profile_image_path: "animated_nayan.jpg",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
-  },
+      "You can contact me directly for any inquiries related to DevOps, cloud infrastructure, automation, CI/CD pipelines, containerization, or open-source development. I typically respond within 24 hours and am open to discussing new opportunities and collaborations."
+  },  
   addressSection: {
     title: "Address",
     subtitle: "Indore, Madhya Pradesh, India",
